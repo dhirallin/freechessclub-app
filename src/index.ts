4e86f5bbede7f526699e7d287bbe1b47263e4a01
Raj Pallin <raj.pallin@gmail.com>
2024-10-16 15:21:57 +1000

    scroll to board

--- src/index.ts
+++ src/index.ts
@@ -6822,6 +6822,7 @@ function setupBoard(game: Game) {
   setColorToMove(game, 'White');
   game.element.find('.setup-board-top').css('display', 'flex');
   game.element.find('.setup-board-bottom').css('display', 'flex');
+  scrollToBoard();
 }
 
 function leaveSetupBoard(game: Game) {
