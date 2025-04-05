92434273984b2bb8f06a4270c0f5fd3d9891b17a
Raj Pallin <raj.pallin@gmail.com>
2025-04-05 19:40:59 +1000

    precache 14

--- src/service-worker.js
+++ src/service-worker.js
@@ -2,20 +2,12 @@ import { precacheAndRoute, matchPrecache, cleanupOutdatedCaches } from 'workbox-
 import { registerRoute } from 'workbox-routing';
 import { NetworkFirst } from 'workbox-strategies';
 
-// pre-cache external resources
+// pre-cache external assets (like CDN fonts and icons)
 const externals = [
-  {"url":"https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css", "revision":"1"},
   {"url":"https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/fonts/fontawesome-webfont.woff2?v=4.7.0", "revision":"1"},
-  {"url":"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css", "revision":"1"},
   {"url":"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/webfonts/fa-solid-900.woff2", "revision":"1"},
   {"url":"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/webfonts/fa-regular-400.woff2", "revision":"1"},
   {"url":"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/webfonts/fa-brands-400.woff2", "revision":"1"},
-  {"url":"https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css", "revision":"1"},
-  {"url":"https://fonts.googleapis.com/css2?family=Noto+Sans+Math&family=Noto+Sans+Symbols+2&display=swap", "revision":"1"},
-  {"url":"https://code.jquery.com/jquery-3.7.0.slim.min.js", "revision":"1"},
-  {"url":"https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js", "revision":"1"},
-  {"url":"https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js", "revision":"1"},
-  {"url":"https://cdn.jsdelivr.net/npm/d3@7.8.0/dist/d3.min.js", "revision":"1"},
   {"url":"https://fonts.gstatic.com/s/notosanssymbols2/v24/I_uyMoGduATTei9eI8daxVHDyfisHr71-vrgfE71.woff2","revision":"1"},
   {"url":"https://fonts.gstatic.com/s/notosansmath/v15/7Aump_cpkSecTWaHRlH2hyV5UEl981w.woff2","revision":"1"},
 ];
@@ -26,10 +18,10 @@ const urlParams = new URLSearchParams(self.location.search);
 if(urlParams.get('env') === 'app') // Capacitor or Electron app, don't cache static assets or use network first strategy
   precacheAndRoute(externals);
 else {
-  // Use network first strategy for html and css files, so we can modify them without having to do cache busting
-  // (by rebuilding and injecting the manifest)
+  // Use network first strategy for html, css and *.bundle.js, so the latest version is always fetched
+  // even before the service-worker is updated
   const networkFirst = new NetworkFirst({
-    cacheName: 'html-css-cache',
+    cacheName: 'network-first-runtime-cache',
     plugins: [{
       cacheWillUpdate: async ({ response }) => {
         return response && response.status === 200 ? response : null;
@@ -38,7 +30,8 @@ else {
   });
 
   registerRoute(
-    ({ request }) => request.destination === 'document' || request.destination === 'style',
+    ({ request }) => request.destination === 'document' || request.destination === 'style'
+        || (request.destination === 'script' && request.url.endsWith('bundle.js')),
     async (options) => {
       let response;
       try { response = await networkFirst.handle(options); }
@@ -57,6 +50,15 @@ else {
 
   // __WB_MANIFEST is injected by inject-manifest.js
   precacheAndRoute([...self.__WB_MANIFEST, ...externals]);
+
+  // Cache first strategy for all other files (not covered by network first or pre-cache routes), 
+  // basically just CDN resources like bootstrap, jquery etc
+  workbox.routing.registerRoute(
+    ({ request }) => true,
+    new workbox.strategies.CacheFirst({
+      cacheName: 'cache-first-runtime-cache',
+    })
+  );
 }
 
 self.addEventListener('install', (event) => {
