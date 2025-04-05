f3f105c268726c9310394aa4fe493b519cff579b
Raj Pallin <raj.pallin@gmail.com>
2025-04-05 19:55:05 +1000

    precache 15

--- src/service-worker.js
+++ src/service-worker.js
@@ -4,10 +4,18 @@ import { NetworkFirst } from 'workbox-strategies';
 
 // pre-cache external assets (like CDN fonts and icons)
 const externals = [
+  {"url":"https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css", "revision":"1"},
   {"url":"https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/fonts/fontawesome-webfont.woff2?v=4.7.0", "revision":"1"},
+  {"url":"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css", "revision":"1"},
   {"url":"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/webfonts/fa-solid-900.woff2", "revision":"1"},
   {"url":"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/webfonts/fa-regular-400.woff2", "revision":"1"},
   {"url":"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/webfonts/fa-brands-400.woff2", "revision":"1"},
+  {"url":"https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css", "revision":"1"},
+  {"url":"https://fonts.googleapis.com/css2?family=Noto+Sans+Math&family=Noto+Sans+Symbols+2&display=swap", "revision":"1"},
+  {"url":"https://code.jquery.com/jquery-3.7.0.slim.min.js", "revision":"1"},
+  {"url":"https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js", "revision":"1"},
+  {"url":"https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js", "revision":"1"},
+  {"url":"https://cdn.jsdelivr.net/npm/d3@7.8.0/dist/d3.min.js", "revision":"1"},
   {"url":"https://fonts.gstatic.com/s/notosanssymbols2/v24/I_uyMoGduATTei9eI8daxVHDyfisHr71-vrgfE71.woff2","revision":"1"},
   {"url":"https://fonts.gstatic.com/s/notosansmath/v15/7Aump_cpkSecTWaHRlH2hyV5UEl981w.woff2","revision":"1"},
 ];
@@ -50,15 +58,6 @@ else {
 
   // __WB_MANIFEST is injected by inject-manifest.js
   precacheAndRoute([...self.__WB_MANIFEST, ...externals]);
-
-  // Cache first strategy for all other files (not covered by network first or pre-cache routes), 
-  // basically just CDN resources like bootstrap, jquery etc
-  workbox.routing.registerRoute(
-    ({ request }) => true,
-    new workbox.strategies.CacheFirst({
-      cacheName: 'cache-first-runtime-cache',
-    })
-  );
 }
 
 self.addEventListener('install', (event) => {
